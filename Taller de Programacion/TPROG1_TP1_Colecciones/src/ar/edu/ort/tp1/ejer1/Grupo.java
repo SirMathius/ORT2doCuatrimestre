7723d041db007dcd2d08b2621d5961748faff6fb
SirMathius <mattslightwolf@gmail.com>
2021-03-17 22:38:11 -0300

    TP1 comienso de tp1 entregable

---
+++ Taller de Programacion/TPROG1_TP1_Colecciones/src/ar/edu/ort/tp1/ejer1/Grupo.java
@@ -0,0 +1,14 @@
+package ar.edu.ort.tp1.ejer1;
+
+import java.util.ArrayList;
+
+public class Grupo {
+
+	//atributos
+	
+	private String nombre;
+	private ArrayList<String> integrantes;
+	
+	
+	
+}
