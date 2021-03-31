4cad6c7695de2d369d023ce947cbb70e1010f2b7
SirMathius <mattslightwolf@gmail.com>
2021-03-31 19:13:36 -0300

    TP1 Clase4 ejemplo (sin terminar)

---
+++ Taller de Programacion/TP1_E_Clase04/src/ar/edu/ort/tp1clase5/compo/Poligono.java
@@ -0,0 +1,53 @@
+package ar.edu.ort.tp1clase5.compo;
+
+import java.util.ArrayList;
+
+public class Poligono {
+
+	private class Coordenada {
+
+		private int x;
+		private int y;
+
+		public Coordenada(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		@Override
+		public String toString() {
+			return "[" + x + ", " + y + "]";
+		}
+	}
+
+	private Coordenada centro;
+	private ArrayList<Coordenada> vertices;
+
+	public Poligono(int x, int y) {
+		centro = new Coordenada(x, y);
+		vertices = new ArrayList<>();
+	}
+
+	public void agregarVertice(int i, int j) {
+		vertices.add(new Coordenada(i, j));
+	}
+
+	public void mostrar() {
+		System.out.printf("El poligono tiene %d lados, con el centro en %s;\n", getCantidadDePuntos(), centro);
+		System.out.print("Sus lados son ");
+		System.out.println(coordenadasFinales());
+	}
+
+	private ArrayList<Coordenada> coordenadasFinales() {
+		ArrayList<Coordenada> cf = new ArrayList<>();
+		for (Coordenada vertice : vertices) {
+			cf.add(new Coordenada(vertice.x + centro.x, vertice.y + centro.y));
+		}
+		return cf;
+	}
+
+	public void moverA(int x, int y) {
+		centro.x = x;
+		centro.y = y;
+	}
+}
