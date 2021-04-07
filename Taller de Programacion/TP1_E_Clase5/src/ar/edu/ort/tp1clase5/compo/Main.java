8b4d3fc139d76e5146d7759bc4ce723fb2b3ff6b
SirMathius <mattslightwolf@gmail.com>
2021-04-07 17:42:38 -0300

    TP1 comienso de ejer. practica de herencia

---
+++ Taller de Programacion/TP1_E_Clase5/src/ar/edu/ort/tp1clase5/compo/Main.java
@@ -0,0 +1,10 @@
+package ar.edu.ort.tp1clase5.compo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Taller de Programacion/TP1_E_Clase5/src/ar/edu/ort/tp1clase5/compo/Vehiculo.java
@@ -0,0 +1,14 @@
+package ar.edu.ort.tp1clase5.compo;
+
+public class Vehiculo {
+	
+	
+	private String marca;
+	private String color;
+	
+	public void conducir() {
+		System.out.println("estoy conduciendo");
+	}
+	
+
+}
